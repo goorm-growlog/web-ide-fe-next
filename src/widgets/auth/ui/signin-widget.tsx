8c2c7eb54e5934550d964754ec7b0d4e9b1288af
sjoon627 <sjoon627@naver.com>
2025-08-23 04:45:55 +0900

    feat: SigninForm 컴포넌트 추가 및 SigninWidget에서 로그인 관련 로직 제거

--- src/widgets/auth/ui/signin-widget.tsx
+++ src/widgets/auth/ui/signin-widget.tsx
@@ -1,43 +1,10 @@
-'use client'
-
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
-import {
-  LoginForm,
-  type PasswordResetData,
-  PasswordResetDialog,
-  SocialLogin,
-  useLoginActions,
-  useLoginForm,
-  usePasswordResetActions,
-  useSocialLogin,
-} from '@/features/auth'
 import AuthLayout from '@/shared/ui/auth-layout'
+import SigninForm from './signin-form'
 
+// 서버 컴포넌트
 const SigninWidget = () => {
-  const [isPasswordResetDialogOpen, setIsPasswordResetDialogOpen] =
-    useState(false)
-
-  const { form } = useLoginForm()
-  const { onSubmit: handleLogin, isLoading } = useLoginActions(form)
-  const { onSocialLogin } = useSocialLogin()
-  const { isLoading: passwordResetLoading, onSubmit: passwordResetAction } =
-    usePasswordResetActions()
-
-  const handlePasswordResetOpen = () => {
-    setIsPasswordResetDialogOpen(true)
-  }
-
-  const handlePasswordReset = async (
-    data: PasswordResetData,
-  ): Promise<void> => {
-    const success = await passwordResetAction(data)
-    if (success) {
-      setIsPasswordResetDialogOpen(false)
-    }
-  }
-
   return (
     <AuthLayout>
       {/* 로고 */}
@@ -50,19 +17,7 @@ const SigninWidget = () => {
           className="w-auto h-10 mx-auto"
         />
       </div>
-      <LoginForm
-        form={form}
-        isLoading={isLoading}
-        onSubmit={handleLogin}
-        onPasswordResetClick={handlePasswordResetOpen}
-      />
-
-      <div className="w-full flex items-center mt-12 mb-4">
-        <hr className="flex-1 border-border" />
-        <span className="mx-4 text-muted-foreground text-sm">or</span>
-        <hr className="flex-1 border-border" />
-      </div>
-      <SocialLogin onSocialLogin={onSocialLogin} />
+      <SigninForm />
       <div className="mt-8 text-center text-muted-foreground text-sm">
         Don't have an account?{' '}
         <Link
@@ -72,12 +27,6 @@ const SigninWidget = () => {
           Sign Up
         </Link>
       </div>
-      <PasswordResetDialog
-        open={isPasswordResetDialogOpen}
-        onOpenChange={setIsPasswordResetDialogOpen}
-        onSubmit={handlePasswordReset}
-        isLoading={passwordResetLoading}
-      />
     </AuthLayout>
   )
 }
