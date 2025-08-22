b2445ffc10e46ed34bc2ce9dea695a183cd02ab6
sjoon627 <sjoon627@naver.com>
2025-08-22 16:19:59 +0900

    feat: 앱 루트에 sonner 토스트 메시지 UI 추가 및 레이아웃 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-
 import './globals.css'
+
+import AppToaster from '@/shared/ui/app-toaster'
+import { Geist, Geist_Mono } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+  <AppToaster />
+  {children}
       </body>
     </html>
   )
