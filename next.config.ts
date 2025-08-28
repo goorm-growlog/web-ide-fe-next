6ea0722614cb592c1c2a1d3785bddd983ea80edb
sjoon627 <sjoon627@naver.com>
2025-08-28 14:22:16 +0900

    fix: API_TARGET 환경변수 설정 수정 및 로그인 결과 타입 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
-const API_TARGET =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080'
+const API_TARGET = process.env.NEXT_PUBLIC_API_BASE_URL
 
 const nextConfig: NextConfig = {
   async rewrites() {
