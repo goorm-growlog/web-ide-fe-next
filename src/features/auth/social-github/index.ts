6bc495082d018c3bafd416a53a9585f83640daec
jaeyeopme <jaeyeopme@gmail.com>
2025-09-13 19:22:30 +0900

    refactor: barrel imports 제거 및 사용하지 않는 파일 정리
    - barrel import 파일들(index.ts) 삭제
    - 사용하지 않는 파일 트리 스토어 제거
    - 더 이상 사용하지 않는 채팅 훅과 목 데이터 파일 삭제

--- src/features/auth/social-github/index.ts
+++
@@ -1,5 +0,0 @@
-// Public API for GitHub social login feature
-
-export { handleGitHubCallback, initiateGitHubAuth } from './api/github-auth'
-export { useGitHubLogin } from './model/use-github-login'
-export { default as GitHubLoginButton } from './ui/github-login-button'
