f49f116bb50c2dd4fd38ff574507a8c612a89044
jaeyeopme <jaeyeopme@gmail.com>
2025-09-16 08:34:13 +0900

    docs: 파일 트리 상수 주석을 한국어로 개선

--- src/features/file-explorer/constants/file-tree-constants.ts
+++ src/features/file-explorer/constants/file-tree-constants.ts
@@ -1,7 +1,8 @@
 /**
- * File Tree Constants
- * Basic constants for file tree operations
+ * 파일 트리 관련 상수 정의
+ * 이전 버전 호환성을 위한 상수들
  */
+
 export const FILE_TREE_CONSTANTS = {
   ROOT_PATH: '/',
 } as const
